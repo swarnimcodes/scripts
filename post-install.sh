d11eddb2c636bd1c1b0716755f364ea2bc753e16
Swarnim B <swarnim14.9@hotmail.com>
2022-12-21 05:45:09 +0530

    Added drivers for gaming (AMD)

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@
 # Adding a comment through my pc as opposed to from github.com
 
 yay -Syyu
-yay -S --needed base base-devel gnome flatpak papirus-icon-theme ttf-overpass ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-code ttf-lato ttf-iosevka-nerd ttf-jetbrains-mono ttf-ibm-plex ttf-ubuntu-font-family ttf-roboto ttf-roboto-mono ttf-font-awesome steam syncthing gnome-software gnome-software-packagekit-plugin jellyfin-bin piper adw-gtk3  
+yay -S --needed base base-devel gnome flatpak papirus-icon-theme ttf-overpass ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-code ttf-lato ttf-iosevka-nerd ttf-jetbrains-mono ttf-ibm-plex ttf-ubuntu-font-family ttf-roboto ttf-roboto-mono ttf-font-awesome steam syncthing gnome-software gnome-software-packagekit-plugin jellyfin-bin piper adw-gtk3 lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 
 flatpak install flathub org.gnome.World.Secrets com.discordapp.Discord net.davidotek.pupgui2 com.brave.Browser com.github.tchx84.Flatseal com.obsproject.Studio com.heroicgameslauncher.hgl org.onlyoffice.desktopeditors md.obsidian.Obsidian io.bassi.Amberol org.gnome.gitlab.somas.Apostrophe com.rafaelmardojai.Blanket dev.Cogitri.Health org.gnome.Solanum com.stremio.Stremio com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.github.wwmm.easyeffects org.gnome.World.Secrets com.belmoussaoui.Obfuscate org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive
 
